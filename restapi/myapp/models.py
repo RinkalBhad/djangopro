3f8b6c79c3fa725c8e4ec0b405e372edf0c289a3
RinkalBhad <139554538+RinkalBhad@users.noreply.github.com>
2024-02-01 21:31:45 -0800

    Delete restapi directory

--- restapi/myapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class userdata(models.Model):
-    firstname=models.CharField(max_length=10)
-    email=models.EmailField()
-    city=models.CharField(max_length=10)
--- restapi/myapp/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import*
-
-class userserializer(serializers.ModelSerializer):
-    class Meta:
-        model=userdata
-        fields="__all__"
